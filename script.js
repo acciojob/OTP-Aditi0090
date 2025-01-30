fc278d994da442d94aff9f4f088682906e14e64f
Aditi0090 <165465425+Aditi0090@users.noreply.github.com>
2025-01-30 14:32:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const codes = document.querySelectorAll(".code");
 
 codes.forEach((input, index) => {
@@ -6,7 +5,11 @@ codes.forEach((input, index) => {
         const nextInput = codes[index + 1];
 
         if (e.target.value.match(/[0-9]/)) {
-            if (nextInput) nextInput.focus();
+            input.classList.add("focused"); // Mark the field as focused
+            if (nextInput) {
+                nextInput.focus();
+                nextInput.classList.add("focused");
+            }
         } else {
             e.target.value = "";
         }
@@ -14,9 +17,20 @@ codes.forEach((input, index) => {
 
     input.addEventListener("keydown", (e) => {
         if (e.key === "Backspace") {
+            input.classList.remove("focused"); // Remove focus class
+
             if (input.value === "" && index > 0) {
                 codes[index - 1].focus();
+                codes[index - 1].classList.add("focused");
             }
         }
+    });
+
+    input.addEventListener("focus", () => {
+        input.classList.add("focused");
+    });
+
+    input.addEventListener("blur", () => {
+        input.classList.remove("focused");
     });
 });
