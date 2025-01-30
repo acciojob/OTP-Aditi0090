cb1e9e91caa74704f8445ddb728d837f8d2098e3
Aditi0090 <165465425+Aditi0090@users.noreply.github.com>
2025-01-30 14:19:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((input, index) => {
+    input.addEventListener("input", (e) => {
+        const nextInput = codes[index + 1];
+
+        if (e.target.value.match(/[0-9]/)) {
+            if (nextInput) nextInput.focus();
+        } else {
+            e.target.value = "";
+        }
+    });
+
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace") {
+            if (input.value === "" && index > 0) {
+                codes[index - 1].focus();
+            }
+        }
+    });
+});
