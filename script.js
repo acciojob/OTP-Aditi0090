c0803dbe75e2fdad1429056a983da61e95dd84e0
Aditi0090 <165465425+Aditi0090@users.noreply.github.com>
2025-01-31 20:58:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const codes = document.querySelectorAll(".code");
+const inputs = document.querySelectorAll('.code');
 
-//     codes.forEach((input, index) => {
-//         input.addEventListener("input", (e) => {
-//             if (e.target.value.match(/[0-9]/)) {
-//                 e.target.classList.add("focused");
-//                 const nextInput = codes[index + 1];
+inputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+        if (input.value !== '') {
+            if (index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        }
+    });
 
-//                 if (nextInput) {
-//                     nextInput.focus();
-//                 }
-//             } else {
-//                 e.target.value = ""; // Clear invalid input
-//             }
-//         });
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && index > 0 && input.value === '') {
+            inputs[index - 1].focus();
+        }
+    });
+});
 
-//         input.addEventListener("keydown", (e) => {
-//             if (e.key === "Backspace") {
-//                 if (e.target.value === "") {
-//                     const prevInput = codes[index - 1];
-//                     if (prevInput) {
-//                         prevInput.focus();
-//                         prevInput.classList.add("focused");
-//                     }
-//                 }
-//             }
-//         });
-
-//         input.addEventListener("focus", () => {
-//             input.classList.add("focused");
-//         });
-
-//         input.addEventListener("blur", () => {
-//             input.classList.remove("focused");
-//         });
-//     });
-// });
+inputs[0].focus();
